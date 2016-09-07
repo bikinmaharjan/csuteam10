cac23d8e13d7111da43172179dbb41bc95a43207
Bikin Maharjan <bikinmaharjan10@gmail.com>
2016-09-07 12:48:33 +1000

    Changes were made according to the comment made by Dreameeer

--- StudentManager.java
+++ StudentManager.java
@@ -31,42 +31,47 @@ public class StudentManager {
         sm = new StudentMap();
         um = new java.util.HashMap<>();
     }
-        
+    
+    //a get method to get Student id from Interface IStudent    
     public IStudent getStudent(Integer id) {
     	IStudent is = sm.get(id);
     	return is != null ? is : createStudent(id);
     }
-
+    
+	//a private get method to get id from XML database 
     private Element getStudentElement(Integer id) {
         for (Element el : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("studentTable").getChildren("student")) 
-            if (id.toString().equals(el.getAttributeValue("sid"))){ 
+            if (id.toString().equals(el.getAttributeValue("sid"))){ //If the student id is present in the database return el 
             	return el;
         }
-        	return null;
+        	return null; //else return null
     }
-                
+    
+    //private methodto create new Student id            
     private IStudent createStudent(Integer id) {
         IStudent is;
         Element el = getStudentElement(id);
-        if (el != null) {
+        if (el != null) { //If user enters a new student Id then add it to the XML
             StudentUnitRecordList rlist = StudentUnitRecordManager.instance().getRecordsByStudent(id);
             is = new Student(new Integer(el.getAttributeValue("sid")),el.getAttributeValue("fname"),el.getAttributeValue("lname"),rlist);    
             sm.put(is.getID(), is);
             return is; 
         }
         
-        throw new RuntimeException("DBMD: createStudent : student not in file");
+        throw new RuntimeException("DBMD: createStudent : student not in file"); //throws Exception run time error if the student file is not present
     }
     
+    //private method to create Student Proxy from Data entered from interface
     private IStudent createStudentProxy(Integer id) {
         Element el = getStudentElement(id);        
         if (el != null) return new StudentProxy(id, el.getAttributeValue("fname"), el.getAttributeValue("lname"));
         throw new RuntimeException("DBMD: createStudent : student not in file");
     }
 
+	//public getter method that gets Students by unit from studentMap class
     public StudentMap getStudentsByUnit(String uc) {
         StudentMap s = um.get(uc);
-        if (s != null) {    
+        if (s != null) {    //if the data from the Object is not null then return s
         	return s;
         }
 
@@ -74,7 +79,7 @@ public class StudentManager {
         IStudent is;
         StudentUnitRecordList ur = StudentUnitRecordManager.instance().getRecordsByUnit(uc);
         
-        for (IStudentUnitRecord S : ur) {            
+        for (IStudentUnitRecord S : ur) {            //loop to loop if the user keeps on entering user data
             is = createStudentProxy(new Integer(S.getStudentID()));
             s.put(is.getID(), is);}
         	um.put( uc, s);
